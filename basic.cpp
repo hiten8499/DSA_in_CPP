b0998acade6b5f100a1f8aae0f01aed528a6328b
hiten8499 <purohithiten49@gmail.com>
2025-03-01 18:53:47 +0530

    Starting with c++ Basics

---
+++ basic.cpp
@@ -0,0 +1,23 @@
+// C++ program to demonstrate the basic syntax
+// Header File Library
+#include <iostream>
+
+// Standard Namespace
+using namespace std;
+
+// Main Function
+int main()
+{
+
+    // Declaration of Variable
+    int num1 = 24;
+    int num2 = 34;
+
+    int result = num1 + num2;
+
+    // Output
+    cout << result << endl;
+
+    // Return Statement
+    return 0;
+}
