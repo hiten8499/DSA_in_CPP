7efdea458c97345f22e23d5748e480a6d5e2674c
hiten8499 <purohithiten49@gmail.com>
2025-03-02 13:58:32 +0530

    DataTypes in C++

--- data_types.cpp
+++ data_types.cpp
@@ -22,9 +22,21 @@ int main(){
      cout<<"\n"<<"Value of Y:-"<<y<<"\n";
 
      //String 
-      string str,str2;
-     cin>>str>>str2;
-      cout<<"Hello \t"<<str<<str2;
-      
+      string str;
+      //cin>>str;
+      cout<<"Hello \t"<<str<<"\n";
+  
+      //if we put space between our string it will not consider as 
+      //single and will store only that part which comes before space
+      string str2;
+      getline(cin,str2);
+
+      cout<<str2;
+           // getline function allows us to take whole line as output
+
+        //char
+
+        char ch='H';
+        cout<<ch;
 return 0;
 }
