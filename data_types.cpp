6d50a105eb62bdf799b9e0643c59438b613d50a1
hiten8499 <purohithiten49@gmail.com>
2025-03-02 13:43:57 +0530

    data_types in cpp

---
+++ data_types.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //Integer DAtaType
+    int x=10;
+
+    //LongDataType
+    
+    long long number=7490918499;
+    //We can store large number in long long data type
+    
+    cout<<number;
+
+    //float,double DataType
+     float a=50.69;
+
+     
+      double y=5;
+        //An integer can also be stored in double output:-5
+     cout<<"\n"<<"Value of Y:-"<<y<<"\n";
+
+     //String 
+      string str,str2;
+     cin>>str>>str2;
+      cout<<"Hello \t"<<str<<str2;
+      
+return 0;
+}
