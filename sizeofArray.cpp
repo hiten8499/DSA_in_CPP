9c470810a8e397d4db75b1a2220d7747326adb94
hiten8499 <purohithiten49@gmail.com>
2025-03-07 20:11:21 +0530

    Size Of Array

---
+++ sizeofArray.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+int myNumbers[5] = {10, 20, 30, 40, 50};
+cout << sizeof(myNumbers);
+
+/*
+int type is usually 4 bytes, 
+so from the code above, 4 x 5 (4 bytes x 5 elements) = 20 bytes. */
+
+}
