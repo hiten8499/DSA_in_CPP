63dc2cbb97a58809c4636fce39b07f3589fd95b8
hiten8499 <purohithiten49@gmail.com>
2025-03-26 20:41:23 +0530

    Find the SIze of given input

--- sizeof.cpp
+++ sizeof.cpp
@@ -7,9 +7,15 @@ using namespace std;
 int main()
 {
     int  input;
+    string input1;
     cout<<"Enter the Integer to find the size:-"<<endl;
     cin>> input;
 
     cout<<endl<<"Size Occupied by Number :-"<<sizeof(input)<<"bytes";
+
+    cout<<"Enter String to find the size:-";
+    cin>>input1;
+    cout<<endl<<"Size Occupied by Number :-"<<sizeof(input)<<"bytes";
+
     return 0;
 }
