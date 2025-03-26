4e340c01225c5055497437d39d8d9f7161ae031b
hiten8499 <purohithiten49@gmail.com>
2025-03-26 20:29:16 +0530

    find  the ASCII VALUE of given Number

---
+++ ASCII.cpp
@@ -0,0 +1,13 @@
+//ASCII VALUE OF CHARACTER
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char input;
+    cout<<"Enter A-Z ,a-z Character to find its ACII VALUE :"<<endl;
+    cin>>input;
+
+    cout<<endl <<"THe ASCII VALUE oF GIVEN NUMBER IS"<<endl<<int(input);
+}
