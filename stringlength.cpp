389d963dbe962bb5792f5c371507013a5e963353
hiten8499 <purohithiten49@gmail.com>
2025-03-27 20:25:03 +0530

    Program to Find the length of the String

---
+++ stringlength.cpp
@@ -0,0 +1,15 @@
+//Write a Program to Find the length of the String
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string input;
+
+    cout<<"Enter a String to Find out Its  Length"<<endl;
+    cin>>input;
+
+    cout<<"The Length of the String :"<<endl<<input.size();
+    return 0;
+}
